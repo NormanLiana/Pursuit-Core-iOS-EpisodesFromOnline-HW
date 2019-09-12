0eae69cc46005b6f4e6f31da694babb7125d71e9
NormanLiana <liananorman@pursuit.org>
2019-09-12 16:18:28 -0400

    adds new swift file for apperror and populates with enum

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/AppError.swift
@@ -0,0 +1,24 @@
+//
+//  AppError.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Liana Norman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case badDataError
+    case badDecoderError
+    case badJSONError
+    case networkError
+    case badHTTPResponse
+    case badURL
+    case noDataError
+    // This is a 404 status code
+    case notFound
+    // Below is 401 and 403
+    case unauthorized
+    case other(rawError: String)
+}
