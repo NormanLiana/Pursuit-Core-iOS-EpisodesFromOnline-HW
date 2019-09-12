6f5961aa80bc4541f50ff51e345b30850cfb9e57
NormanLiana <liananorman@pursuit.org>
2019-09-12 16:15:45 -0400

    adds Network Manager code to corresponding file

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/NetworkManager.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/NetworkManager.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    
+    // TODO - later update 'private init' to cache
+    private init() {}
+    static let shared = NetworkManager()
+    
+    // Performs GET requests
+    // Parameters: URL as a string
+    // Completion: Result with Data in success, AppError in failure
+    func fetchData(urlString: String, completionHandler: @escaping (Result<Data, AppError>) -> () ) {
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                completionHandler(.failure(.networkError))
+                return
+            }
+            guard let data = data else {
+                completionHandler(.failure(.noDataError))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completionHandler(.failure(.badHTTPResponse))
+                return
+            }
+            switch response.statusCode {
+            case 404:
+                completionHandler(.failure(.notFound))
+            case 401, 403:
+                completionHandler(.failure(.unauthorized))
+            case 200...299:
+                completionHandler(.success(data))
+            default:
+                completionHandler(.failure(.other(rawError: "Wrong status code")))
+            }
+            }.resume()
+    }
+}
