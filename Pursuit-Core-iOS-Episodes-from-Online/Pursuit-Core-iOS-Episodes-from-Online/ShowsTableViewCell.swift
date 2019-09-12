bee6d0221af00f65642ba9532a1b74870b96adeb
NormanLiana <liananorman@pursuit.org>
2019-09-12 09:42:05 -0400

    adds TVCell to first VC and creates cocoa file and sets TVCell to that class

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShowsTableViewCell.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Liana Norman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ShowsTableViewCell: UITableViewCell {
+
+
+}
