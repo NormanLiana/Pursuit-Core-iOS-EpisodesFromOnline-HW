9e9ebc9eabb0d80d33b080ca9d39fc6320585b93
NormanLiana <liananorman@pursuit.org>
2019-09-12 15:34:25 -0400

    adds image view 2 labels and text view to 3rd VC on main storyboard

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/EpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Liana Norman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
