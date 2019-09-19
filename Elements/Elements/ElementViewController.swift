6e19443361f93e5422025962628ab2b8119ef5da
NormanLiana <liananorman@pursuit.org>
2019-09-19 10:40:08 -0400

    adds cocoa touch file for element VC and adds VC to main storyboard

---
+++ Elements/Elements/ElementViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ElementViewController.swift
+//  Elements
+//
+//  Created by Liana Norman on 9/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ElementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
