77f7ced073ae08264b04527bc695b6223f391781
NormanLiana <liananorman@pursuit.org>
2019-09-19 10:46:33 -0400

    adds customTV cell with cocoa touch file as well

---
+++ Elements/Elements/TableViewCells/ElementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ElementTableViewCell.swift
+//  Elements
+//
+//  Created by Liana Norman on 9/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ElementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
