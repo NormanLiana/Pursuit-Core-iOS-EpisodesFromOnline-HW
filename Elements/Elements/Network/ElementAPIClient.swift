17b939113ae0b2fa678ad2dbdba5c6808de44df2
NormanLiana <liananorman@pursuit.org>
2019-09-19 11:51:17 -0400

    adds and populates API Client file, as well as image helper and element model

---
+++ Elements/Elements/Network/ElementAPIClient.swift
@@ -0,0 +1,61 @@
+//
+//  ElementAPIClient.swift
+//  Elements
+//
+//  Created by Liana Norman on 9/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+class ElementAPIManager {
+    private init() {}
+    
+    static let shared = ElementAPIManager()
+    
+    func getElements(completionHandler: @escaping (Result<[Element], AppError>) -> () ) {
+        let urlStr = ""
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                    let elementInfo = try JSONDecoder().decode(Element.self, from: data)
+                    
+                    completionHandler(.success([elementInfo]))
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+    
+    func postElement(element: Element, completionHandler: @escaping (Result<Data, AppError>) -> () ) {
+        
+        guard let encodedData = try? JSONEncoder().encode(element) else {
+            fatalError("encoder failed")
+        }
+        let urlStr = ""
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        NetworkHelper.manager.performDataTask(withUrl: url, andHTTPBody: encodedData, andMethod: .post) { (result) in
+            switch result {
+            case .success(let data):
+                completionHandler(.success(data))
+            case .failure(let error):
+                print(error)
+                
+            }
+        }
+    }
+    
+    
+}
---
+++ Elements/ElementsTests/ElementsTests.swift
@@ -0,0 +1,23 @@
+//
+//  ElementsTests.swift
+//  ElementsTests
+//
+//  Created by Liana Norman on 9/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import XCTest
+
+class ElementsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+
+}
