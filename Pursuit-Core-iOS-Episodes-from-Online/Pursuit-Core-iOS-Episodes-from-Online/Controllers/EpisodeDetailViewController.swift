d59abd6535f215afea74d1861af2afbd733a90cf
NormanLiana <liananorman@pursuit.org>
2019-09-23 09:55:52 -0400

    sets up views for episodeDVC

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/EpisodeDetailViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/EpisodeDetailViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class EpisodeDetailViewController: UIViewController {
 
     // MARK: Outlets
+    @IBOutlet weak var episodeImage: UIImageView!
+    @IBOutlet weak var episodeName: UILabel!
+    @IBOutlet weak var seasonAndEpisodeLabel: UILabel!
+    @IBOutlet weak var episodeSummary: UITextView!
     
     // MARK: Properties
     var episode: Episode!
@@ -18,7 +22,26 @@ class EpisodeDetailViewController: UIViewController {
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
     }
     
+    // MARK: Private Methods
+    private func setupViews() {
+        episodeName.text = episode.name
+        episodeSummary.text = episode.summary
+        seasonAndEpisodeLabel.text = episode.seasonAndEpisode
+        if let urlStr = episode.image?.original {
+            ImageHelper.shared.fetchImage(urlImage: urlStr) { (result) in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let imageFromOnline):
+                        self.episodeImage.image = imageFromOnline
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
 
 }
